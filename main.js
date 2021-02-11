3b7fb9bcc2ef2419db07a5a962839f31096c5a8d
Marwane Benharoun <71384548+MBenharoun@users.noreply.github.com>
2021-02-11 21:09:57 +0100

    Demo Final

--- main.js
+++ main.js
@@ -570,12 +570,14 @@ function onDeath(){
 	open('death.html',"_self");
 }
 function restart(){
-
 	open('game.html',"_self");
 }
 function startG(){
 	open("start.html","_self");
 
+}
+function startgame(){
+	setTimeout(restart,5000);
 }
 function credit(){
 	open('index.html',"_self");
